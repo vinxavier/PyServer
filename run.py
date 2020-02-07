9b8de91fc677b8f365a154801b5c2a3d1e60fd72
Vinicius <vsxavier@live.com>
2020-02-07 16:02:20 -0300

    Tenta corrigir gunicorn 9

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+app = Flask(__name__)
 
-    return app
+from app import api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
 
+return app
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+app.run(debug=True)
