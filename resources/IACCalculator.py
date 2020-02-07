68bd1b029f551f600230d515b1db478ebeeafc77
Vinicius <vsxavier@live.com>
2020-02-07 14:07:55 -0300

    Adiciona mensagem de condição do paciente na resposta de IAC

--- resources/IACCalculator.py
+++ resources/IACCalculator.py
@@ -4,9 +4,42 @@ from flask_restful import Resource
 class IACCalculator(Resource):
     def post(self):
         json_data = request.get_json(force=True)
-        sexo = json_data['sexo']
         circ_quadril = json_data['circ_quadril']
         altura = json_data['altura']
-        iac_divisor = altura * ( altura ** (1/2) )
-        iac = ( circ_quadril / iac_divisor ) - 18
-        return jsonify(iac = iac)
+        sexo = json_data['sexo']
+        if self.__is_valid(circ_quadril, altura, sexo):
+            iac = self.__calc_iac(circ_quadril, altura)
+            condicao = self.__condition(iac, sexo)
+            return jsonify(iac = iac, condicao = condicao)
+        else:
+            error = "Paramêtros inválidos"
+            return jsonify(error = error)
+
+    def __calc_iac(self, circ, altura):
+        iac_divisor = altura ** (1.5) 
+        return ( circ / iac_divisor ) - 18
+
+    def __condition(self, iac, sexo):
+        if sexo == "m":
+            if iac < 8:
+                return "Abaixo do normal"
+            elif 8 <= iac < 21:
+                return "Normal"
+            elif 21 <= iac < 25:
+                return "Sobrepeso"
+            else:
+                return "Obesidade"
+        elif sexo == "f":
+            if iac < 21:
+                return "Abaixo do normal"
+            elif 21 <= iac < 33:
+                return "Normal"
+            elif 33 <= iac < 38:
+                return "Sobrepeso"
+            else:
+                return "Obesidade"
+        else:
+            return "ERROR"
+    
+    def __is_valid(self, circ, altura, sexo):
+        return circ > 0 and altura > 0 and (sexo == "m" or sexo == "f")
