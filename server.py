31e4caada130627a7ceb2669c0becca07fbee715
Vinicius <vsxavier@live.com>
2020-02-07 16:40:17 -0300

    Tenta corrigir gunicorn 18

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -10,5 +11,6 @@ def create_app():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
